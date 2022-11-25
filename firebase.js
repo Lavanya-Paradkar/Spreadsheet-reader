62820637dd2d50541ab9cf48ace7f6d8a3d90c7b
unknown <paradkarlavanya@gmail.com>
2022-11-25 14:25:42 +0530

    deployed

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiX6frmvsQdfXg_dCULNliufLCLSMfDIc",
+  authDomain: "spreadsheet-reader-dc4b2.firebaseapp.com",
+  projectId: "spreadsheet-reader-dc4b2",
+  storageBucket: "spreadsheet-reader-dc4b2.appspot.com",
+  messagingSenderId: "853118774152",
+  appId: "1:853118774152:web:ef7c0f0cf490fd5ce1683d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
